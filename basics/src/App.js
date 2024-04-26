f0f8b513712a0243408bcdbb72c94d936be9e46f
Avanthi123456 <105709096+Avanthi123456@users.noreply.github.com>
2024-04-26 15:03:43 +0530

    HelloKiew

---
+++ basics/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloKiew from './components/HelloKiew';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        
+         <HelloKiew/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ basics/src/components/HelloKiew.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HelloKiew() {
+    return (
+        <h1>Hello Kiew!</h1>
+    );
+}
+
+export default HelloKiew;
